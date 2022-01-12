3bcd447dc88489c38daef562f02cd032abd46007
Justin Davis <jrcd83@gmail.com>
2022-01-11 21:05:28 -0500

    Finish problem 29 redux

--- erlang/euler29.erl
+++ erlang/euler29.erl
@@ -1,51 +1,36 @@
 -module(euler29).
 -export([smart/1,brute/1]).
 
-%% Like APL's scan operator ("\").
-scan(Fun, [Hd|Tl]) ->
-    scan(Fun,Hd,Tl,[]).
-scan(_, X, [], L) ->
-    lists:reverse([X|L]);
-scan(Fun, X, [Y|L1], L2) ->
-    scan(Fun, Fun(X,Y), L1, [X|L2]).
-
-dump(Is, Tbl) ->
-    lists:foreach(fun({I,L}) ->
-                          Str = [$0 + D || D <- L],
-                          io:format("\t~02.B: ~s~n", [I, Str])
-                  end, lists:zip(Is, Tbl)).
-
-smart([Max]) ->
-    MaxB = list_to_integer(Max),
-    %% MaxA = floor(math:sqrt(MaxB)),
-    MaxA = floor(math:log(MaxB) / math:log(2)),
-    As = lists:seq(2,MaxA),
-    Bs = lists:seq(2,MaxB),
-    %% NOTE: Squares are not dups! If 2^2 is within range remember 4^1 is not calculated!
-    Tbl1 = [[if A >= B -> 0; B rem A /= 0 -> 0; true -> 1 end || B <- Bs]
-            || A <- As],
-    %% io:format("Tbl1\t~lp~n", [Tbl1]),
-    io:format("Tbl1~n", []),
-    dump(As, Tbl1),
-    Tbl2 = scan(fun(L1, L2) ->
-                        lists:zipwith(fun(B1,B2) -> B1 + B2 end, L1, L2)
-                end, Tbl1),
-    %% io:format("Tbl2\t~lp~n", [Tbl2]),
-    io:format("Tbl2~n", []),
-    dump(As, Tbl2),
-    PowDups = [lists:sum(L) || L <- Tbl2],
-    io:format("PowDups\t~lp~n", [PowDups]),
-    %% Pows = [floor(math:log(MaxB) / math:log(A)) || A <- As],
-    HasPows = lists:seq(2, floor(math:sqrt(MaxB))),
-    io:format("HasPows\t~lp~n", [HasPows]),
-    Pows = [floor(math:log(MaxB) / math:log(I)) || I <- HasPows],
-    io:format("Pows\t~lp~n", [Pows]),
-    Dups = [lists:nth(I-1, PowDups) || I <- Pows],
-    io:format("Dups\t~lp~n", [Dups]),
-    io:format("Answer\t~B~n", [(MaxB-1) * (MaxB-1) - lists:sum(Dups)]).
-
 brute([L]) ->
     Max = list_to_integer(L),
     Seq = lists:seq(2,Max),
-    X = length(lists:usort([floor(math:pow(A,B)) || A <- Seq, B <- Seq])),
-    io:format("Answer\t~B~n", [X]).
+    length(lists:usort([floor(math:pow(A,B)) || A <- Seq, B <- Seq])).
+
+smart([L]) ->
+    N = list_to_integer(L),
+    Bases = uniq_bases(N),
+    Dups = dups(floor(math:log2(N)), N),
+    io:format("Reduced bases: ~p~n", [Bases]),
+    io:format("Dups: ~p~n", [Dups]),
+    AllDups = lists:sum([lists:nth(I,Dups) || I <- lists:append(Bases)]),
+    (N-1)*(N-1) - AllDups.
+
+dups(MaxP, MaxN) ->
+    N = lists:seq(2,MaxN),
+    Prods = [sets:from_list([P*I || I <- N]) || P <- lists:seq(1,MaxP)],
+    Fun = fun (S1, S2) ->
+                  {sets:intersection(S1,S2), sets:union(S1,S2)}
+          end,
+    {L,_} = lists:mapfoldl(Fun,sets:new(),Prods),
+    [sets:size(S) || S <- L].
+
+log(N,M) -> floor(math:log(M) / math:log(N)).
+
+uniq_bases(N) -> uniq_bases(lists:seq(2,floor(math:sqrt(N))), N, []).
+uniq_bases([], _, L) -> L;
+uniq_bases([I|Is], N, L) ->
+    Log = log(I,N),
+    Pows = lists:seq(1,Log),
+    {Vals,_} = lists:mapfoldl(fun (X,Y) -> {Y,X*Y} end, I,
+                              lists:duplicate(Log-1,I)),
+    uniq_bases(Is -- Vals, N, [Pows|L]).

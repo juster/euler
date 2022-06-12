af51ff3a13bce686cf1eeac7a12873176fbe0427
Justin Davis <jrcd83@gmail.com>
2022-06-12 08:42:35 -0400

    Complicate zig solution with a 2D matrix

--- zig/euler31.zig
+++ zig/euler31.zig
@@ -2,18 +2,26 @@ const print = @import("std").debug.print;
 const coins = [_]u64{1,2,5,10,20,50,100,200};
 const n     = 200;
 
-var count: [n+1]u64 = .{1} ++ .{0}**n;
+var count: [coins.len*(n+1)]u64 = .{0}**(coins.len*(n+1));
 
 pub fn main() void {
-    var i: u64 = 1;
+    var mat = @ptrCast(*[coins.len][n+1]u64, &count);
 
-    for (coins) |c| {
+    var i: usize = 0;
+    while (i <= n) : (i += 1) {
+        mat[0][i] = 1;
+    }
+
+    for (coins[1..coins.len]) |c,j| {
+        // j starts at 0
+        mat[j+1][0] = 1;
         i = 1;
         while (i <= n) : (i += 1) {
+            mat[j+1][i] = mat[j][i];
             if (c <= i) {
-                count[i] += count[i-c];
+                mat[j+1][i] += mat[j+1][i-c];
             }
         }
     }
-    print("Problem 31 answer: {}\n", .{count[n]});
+    print("Problem 31 answer: {}\n", .{count[count.len-1]});
 }

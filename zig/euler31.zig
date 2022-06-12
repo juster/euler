55ea9c4204fcaf47b78d53ff9e8aee8132b97f5b
Justin Davis <jrcd83@gmail.com>
2022-06-12 08:52:18 -0400

    Try variant without coin sum of 0 placeholder

--- zig/euler31.zig
+++ zig/euler31.zig
@@ -2,24 +2,24 @@ const print = @import("std").debug.print;
 const coins = [_]u64{1,2,5,10,20,50,100,200};
 const n     = 200;
 
-var count: [coins.len*(n+1)]u64 = .{0}**(coins.len*(n+1));
+var count: [coins.len*n]u64 = .{0}**(coins.len*n);
 
 pub fn main() void {
-    var mat = @ptrCast(*[coins.len][n+1]u64, &count);
+    var mat = @ptrCast(*[coins.len][n]u64, &count);
 
     var i: usize = 0;
-    while (i <= n) : (i += 1) {
+    while (i < n) : (i += 1) {
         mat[0][i] = 1;
     }
 
     i = 1;
     for (coins[1..coins.len]) |c| {
-        mat[i][0] = 1;
-
         var j: usize = 1;
-        while (j <= n) : (j += 1) {
+        while (j < n) : (j += 1) {
             mat[i][j] = mat[i-1][j];
-            if (c <= j) {
+            if (c == j) {
+                mat[i][j] += 1;
+            } else if (c < j) {
                 mat[i][j] += mat[i][j-c];
             }
         }

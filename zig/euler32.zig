fcd04d285a043eb1bc22c1da04e1d5a70d4c4b5f
Justin Davis <jrcd83@gmail.com>
2022-06-16 22:08:38 -0400

    Correct answer for problem 32

--- zig/euler32.zig
+++ zig/euler32.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const fmt = std.fmt;
+const math = std.math;
+const log10 = std.math.log10;
+const pow = std.math.pow;
 const print = std.debug.print;
 
 pub fn main() !void {
@@ -9,97 +13,59 @@ pub fn main() !void {
     var found = std.AutoHashMap(u64, void).init(allocator);
     defer found.deinit();
 
-    var digbuf: [9]u8 = .{1,1,0,0,0,0,0,0,0};
-    var i  = digbuf[0..2];
-    var j  = digbuf[2..5];
-    var ij = digbuf[0..5];
-    var y  = digbuf[5..];
+    var i: u64 = 2;
 
-    while (true) {
-        i[0] += 1;
-        normalize(i) catch |over| switch (over) {
-            error.Overflow => break
-        };
-        if (!good(i)) continue;
+    while (i < 1e9) : (i += 1) {
+        var seen: [10]bool = .{false}**10;
+        if (!good(i, seen[0..])) continue;
 
-        j[0] = 1; j[1] = 0; j[2] = 1;
-        while (true) {
-            j[0] += 1;
-            normalize(j) catch |over| switch (over) {
-                error.Overflow => break
-            };
-            if (!good(ij)) continue;
+        var j: u64 = 101;
+        const p = -(2.0 * log10(@intToFloat(f32, i)) - 9.0) / 2.0;
+        const max = @floatToInt(u64, pow(f32, 10, p));
+        if (max <= 1) break;
 
-            var x = arbToInt(i) * arbToInt(j);
-            if (x > 9999) continue;
-            if (intToArb(x, y) < 4) continue;
-            if (!good(digbuf[0..])) continue;
+        while (j <= max) : (j += 1) {
+            var seen2 = seen;
+            if (!good(j, seen2[0..])) continue;
 
-            print("{} x {} = {}\n",
-                  .{arbToInt(i), arbToInt(j), x});
+            const x = i * j;
+            if (!good(x, seen2[0..])) continue;
+
+            var c: u8 = 0;
+            for (seen2) |b| c += @boolToInt(b);
+            if(c != 9) continue;
+
+            print("{} x {} = {}\n", .{i, j, x});
             try found.put(x, {});
         }
     }
 
     var iter = found.iterator();
     var sum: u64 = 0;
+    print("\n", .{});
     while (iter.next()) |entry| {
+        print("{}\n", .{entry.key_ptr.*});
         sum += entry.key_ptr.*;
     }
-    print("{}\n", .{sum});
+    print("Sum {}\n", .{sum});
 }
 
-const ArbError = error {
-    Overflow,
-};
-
-fn normalize(digits: []u8) ArbError!void {
-    var i: usize = 0;
-    while (i < digits.len-1) : (i += 1) {
-        if (digits[i] >= 10) {
-            digits[i+1] += digits[i] / 10;
-            digits[i] = digits[i] % 10;
-        } else {
-            break;
-        }
-    }
-    if (digits[digits.len-1] >= 10) {
-        return ArbError.Overflow;
-    }
-}
-
-fn good(digits: []u8) bool {
-    var seen: [10]bool = .{false}**10;
-    for (digits) |d| {
-        if (seen[d]) {
+fn good(i: u64, seen: []bool) bool {
+    var digbuf: [10]u8 = .{0}**10;
+    const n = formatInt(digbuf[0..], i);
+    for (digbuf[0..n]) |d| {
+        const j = d-'0';
+        if (seen[j]) {
             return false;
         } else {
-            seen[d] = true;
+            seen[j] = true;
         }
     }
     return !seen[0];
 }
 
-fn arbToInt(digits: []u8) u64 {
-    var x: u64 = 0;
-    var i: usize = digits.len;
-    while (i > 0) {
-        i -= 1;
-        x *= 10;
-        x += digits[i];
-    }
-    return x;
-}
-
-fn intToArb(n: u64, dest: []u8) usize {
-    var i: usize = 0;
-    var m = n;
-    while (m > 0) : (i += 1) {
-        dest[i] = @intCast(u8, (m % 10) & 0xFF);
-        m /= 10;
-    }
-    while (i < dest.len) : (i += 1) {
-        dest[i] = 0;
-    }
-    return i;
+fn formatInt(buf: []u8, i: u64) usize {
+    return fmt.formatIntBuf(buf, i, 10,
+                            fmt.Case.lower,
+                            fmt.FormatOptions {});
 }

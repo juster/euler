b07e4bcc4bfa5d41b155f7e5ecf60e9a070c70b3
Justin Davis <jrcd83@gmail.com>
2022-06-18 12:20:04 -0400

    Simpler answer to problem 34 using std.fmt

--- zig/euler34.zig
+++ zig/euler34.zig
@@ -1,43 +1,23 @@
 const std = @import("std");
 const print = std.debug.print;
+const fmt = std.fmt;
 
 pub fn main() !void {
-    var fac: [10]u64 = .{1} ++ (.{0} ** 9);
-    var i: usize = 1;
-    while (i < fac.len) : (i += 1) fac[i] = fac[i-1] * i;
-    print("{d}\n", .{fac});
-
     const max = 5;
+    var fac: [10]u64 = .{1} ++ (.{0} ** 9);
+
+    var i: u64 = 1;
+    while (i < fac.len) : (i += 1) fac[i] = fac[i-1] * i;
+    // print("{d}\n", .{fac});
+
     var d: [max]u8 = .{0} ** max;
-    var n: usize = 1;
 
-    d[0] = 3;
-    while (true) {
-        var a: u64 = 0;
-        var c: u64 = 1;
+    i = 3;
+    while (i < 50000) : (i += 1) {
+        const n = fmt.formatIntBuf(&d, i, 10, fmt.Case.lower, .{});
         var facsum: u64 = 0;
-        for (d[0..n]) |b| {
-            facsum += fac[b];
-            a += b * c;
-            c *= 10;
-        }
+        for (d[0..n]) |b| facsum += fac[b-'0'];
         // print("{} {}\n", .{a, facsum});
-        if (a == facsum) print("{}\n", .{a});
-
-        d[0] += 1;
-        if (carry(&d, n)) |m| n = m else break;
+        if (i == facsum) print("{}\n", .{i});
     }
-}
-
-fn carry(buf: []u8, count: usize) ?usize {
-    var n = count;
-    var j: usize = 0;
-    while (j < n) : (j += 1) {
-        if (buf[j] < 10) break;
-        if (j == buf.len-1) return null;
-        buf[j+1] += buf[j] / 10;
-        buf[j+0] %= 10;
-        if (j == n-1) n += 1;
-    }
-    return n;
 }

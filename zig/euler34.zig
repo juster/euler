994a7a7e4972bb667d23b7c506893210368178e2
Justin Davis <jrcd83@gmail.com>
2022-06-19 09:13:12 -0400

    Minor tweaks to 34

--- zig/euler34.zig
+++ zig/euler34.zig
@@ -3,17 +3,14 @@ const print = std.debug.print;
 const fmt = std.fmt;
 
 pub fn main() !void {
-    const max = 5;
-    var fac: [10]u64 = .{1} ++ (.{0} ** 9);
-
     var i: u64 = 1;
+    var fac: [10]u64 = .{1} ++ (.{0} ** 9);
     while (i < fac.len) : (i += 1) fac[i] = fac[i-1] * i;
     // print("{d}\n", .{fac});
 
-    var d: [max]u8 = .{0} ** max;
-
+    var d: [10]u8 = .{0} ** max;
     i = 3;
-    while (i < 50000) : (i += 1) {
+    while (i < fac[9]) : (i += 1) {
         const n = fmt.formatIntBuf(&d, i, 10, fmt.Case.lower, .{});
         var facsum: u64 = 0;
         for (d[0..n]) |b| facsum += fac[b-'0'];

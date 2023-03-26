8635ae621a0ffc8d08192ea49203570e7ad8a89c
Justin Davis <jrcd83@gmail.com>
2023-03-26 09:37:18 -0400

    35: Rotate the number only once
    Use a slice to link rotations together.

--- go/euler35.go
+++ go/euler35.go
@@ -5,30 +5,37 @@ import "fmt"
 const limit = 1000000
 
 func main() {
-	c := 0
-	sieve := eratosthenes(limit)
-	Outer: for i := 1; i < limit; i++ {
-		s := fmt.Sprintf("%d", i)
-		var x int
-		for i := 0; i < len(s); i++ {
-			fmt.Sscanf(s, "%d", &x)
-			if(!sieve.isPrime(x)){
-				continue Outer
-			}
-			rotateLeft(&s);
+	primeSieve := eratosthenes(limit)
+	rotated := make([]int, limit+1)
+	for i := 1; i <= limit; i++ {
+		if primeSieve.isPrime(i) {
+			j := rotateLeft(i)
+			rotated[i] = j
+		} else {
+			rotated[i] = 0
 		}
-		c++
 	}
-	fmt.Printf("Result: %d\n", c)
+	n := 0
+	for i := 1; i <= limit; i++ {
+		for j := rotated[i]; j > 0; j = rotated[j] {
+			if j == i {
+				n++
+				break
+			}
+		}
+	}
+	fmt.Printf("Result: %d\n", n)
 }
 
-func rotateLeft(ptr *string) {
-	s := *ptr
+func rotateLeft(i int) int {
+	var x int
+	s := fmt.Sprintf("%d", i)
 	slice := []byte(s)
-	b := slice[0]
-	for i := 1; i < len(slice); i++ {
-		slice[i-1] = slice[i]
+	if len(slice) > 1 && slice[1] == '0' {
+		// Special case: rotating this number changes the number of digits
+		// in the result and is invalid.
+		return 0
 	}
-	slice[len(slice)-1] = b
-	*ptr = string(slice)
+	fmt.Sscanf(string(append(slice[1:len(slice)], slice[0])), "%d", &x)
+	return x
 }

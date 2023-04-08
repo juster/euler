f61f12edb5a305c0fa6508ce09c46394a72d5c7d
Justin Davis <jrcd83@gmail.com>
2023-04-07 22:39:27 -0400

    42: Simplify a bit

--- go/euler42.go
+++ go/euler42.go
@@ -8,42 +8,31 @@ import (
 )
 
 func main() {
-	wchan := make(chan []byte)
 	schan := make(chan int)
-	go words(wchan)
-	go wordsums(schan, wchan)
+	go func(){
+		buf, err := os.ReadFile("p042_words.txt")
+		if err != nil {
+			panic(err)
+		}
+		for _, w := range bytes.Split(buf, []byte(",")) {
+			n := 0
+			for i := 1; i < len(w)-1; i++ {
+				n += int(w[i] - byte('A') + 1)
+			}
+			schan <-n
+		}
+		close(schan)
+	}()
 
 	c := 0
 	for s := range schan {
 		// x = (n*(n+1))/2 = (n^2 + n)/2
 		// => n^2 + n - 2x = 0
-		x := (-1.0 - math.Sqrt(1.0+8.0*float64(s))) / 2.0
+		x := (-1.0 - math.Sqrt(1.0 + 8.0 * float64(s))) / 2.0
 		if _, f := math.Modf(x); f == 0.0 {
 			c++
 		}
 	}
 
 	fmt.Println("Results:", c)
-}
-
-func words(out chan<- []byte) {
-	buf, err := os.ReadFile("p042_words.txt")
-	if err != nil {
-		panic(err)
-	}
-	for _, w := range bytes.Split(buf, []byte(",")) {
-		out <- w
-	}
-	close(out)
-}
-
-func wordsums(out chan<- int, in <-chan []byte) {
-	for w := range in {
-		n := 0
-		for i := 1; i < len(w)-1; i++ {
-			n += int(w[i] - byte('A') + 1)
-		}
-		out <- n
-	}
-	close(out)
 }

04db77ff174852dfd2869ba5bc266f5fb747882c
Justin Davis <jrcd83@gmail.com>
2023-04-13 18:05:35 -0400

    45: Brute channel solution

---
+++ go/euler45.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	triCh, pentaCh, hexCh := make(chan int), make(chan int), make(chan int)
+	go genTri(triCh)
+	go genPenta(pentaCh)
+	go genHexa(hexCh)
+	ff(285, triCh)
+	ff(165, pentaCh)
+	ff(143, hexCh)
+	x, y, z := <-triCh, <-pentaCh, <-hexCh
+
+	for {
+		if x < y {
+			x = <-triCh
+			continue
+		}
+		if y < z {
+			y = <-pentaCh
+			continue
+		}
+		if z < y {
+			z = <-hexCh
+			continue
+		}
+		fmt.Println("Results:", x, y, z)
+		break
+	}
+}
+
+func ff(n int, ch chan int) {
+	for i := 0; i < n; i++ {
+		<-ch
+	}
+}
+
+func genTri(out chan int) {
+	x := 1
+	y := 1
+	for {
+		out <-x
+		y += 1
+		x += y
+	}
+}
+
+func genPenta(out chan int) {
+	x := 1
+	y := 3
+	for {
+		out <-x
+		y += 3
+		x += y - 2
+	}
+}
+
+func genHexa(out chan int) {
+	x := 1
+	y := 0
+	for {
+		out <-x
+		y += 4
+		x += y + 1
+	}
+}

d24a394d53f2713e7a1c01119c6062b2ef51f9dd
Justin Davis <jrcd83@gmail.com>
2023-04-20 21:55:05 -0400

    45: make even simpler for no reason

--- go/euler45.go
+++ go/euler45.go
@@ -5,16 +5,15 @@ import (
 )
 
 func main() {
-	ch3, ch5, ch6 := gen(3, 285), gen(5, 165), gen(6, 143)
-	x, y, z := <-ch3, <-ch5, <-ch6
+	ch5, ch6 := gen(5, 165), gen(6, 143)
+	y, z := <-ch5, <-ch6
 
 	for {
 		switch {
-		case x < y: x = <-ch3; continue
 		case y < z: y = <-ch5; continue
 		case z < y: z = <-ch6; continue
 		}
-		fmt.Println("Results:", x, y, z)
+		fmt.Println("Results:", y, z)
 		break
 	}
 }
@@ -23,16 +22,11 @@ func gen(shape, ff int) chan int {
 	out := make(chan int)
 	shape -= 2
 	go func(x, y int){
-		for {
-			out <-x
+		for i := 1; ; i++ {
+			if i > ff { out <-x }
 			y += shape
 			x += y + 1
 		}
 	}(1, 0)
-
-	// fast-forward a certain amount
-	for i := 0; i < ff; i++ {
-		_ = <-out
-	}
 	return out
 }

279b5a3232c2ca571cf2133754e383665bbca1fd
Justin Davis <jrcd83@gmail.com>
2023-04-22 10:16:28 -0400

    47: Simple brute force
    Started with a prime sieve but read 1 post on the forum which showed it's not
    even needed.

---
+++ go/euler47.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const count = 4
+
+func main() {
+	var n, m int
+	for i := 4; ; i++ {
+		m = 0
+		x, max := i, int(math.Sqrt(float64(i)))
+		// No need for primality checks or sieves.
+		// -Barker on the problem forum
+		for j := 2; j <= max; j++ {
+			switch {
+			case x < j:
+				break
+			case x%j == 0:
+				m++
+				x /= j
+				for x%j == 0 {
+					x /= j
+				}
+			}
+		}
+		if x > 1 {
+			// what remains is a prime factor > sqrt(x)
+			m++
+		}
+		if m != count {
+			n = 0
+			continue
+		}
+		if n++; n == count {
+			fmt.Println("Result:", i-count+1)
+			return
+		}
+	}
+}

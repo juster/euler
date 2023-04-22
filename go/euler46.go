c88103f5101526c99faec9deeffa56bf3488c3c3
Justin Davis <jrcd83@gmail.com>
2023-04-21 23:25:35 -0400

    46: Smarter brute force

--- go/euler46.go
+++ go/euler46.go
@@ -2,33 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 const limit = 10000
 
 var sieve = eratosthenes(limit)
-var found = make([]bool, limit+1)
 
 func main() {
-	var n, i, x, y int = 0, 1, 0, 0
-	for {
-		if x = 2 * i * i; x > limit { break }
-		for p := 3; p < limit; p += 2 {
-			if !sieve.isPrime(p) { continue }
-			y = x + p
-			switch{
-			case y > limit || sieve.isPrime(y):
-			case y % 2 == 0:
-			default: found[y] = true; n++
-			}
-		}
-		i++
-	}
-	fmt.Println("Composite numbers:", n)
-	for i := 3; i < len(found); i += 2 {
+	var i, j, k int
+	// For every odd composite number, i.
+	CompositeLoop:
+	for i = 9; i < limit; i += 2 {
 		if sieve.isPrime(i) { continue }
-		if found[i] { continue }
+		// ... and every prime number j smaller than i
+		for j = 1; j < i; j += 2 {
+			if !sieve.isPrime(j) { continue }
+			if k = i - j; k % 2 == 1 { continue }
+			if _, f := math.Modf(math.Sqrt(float64(k / 2))); f != 0.0 { continue }
+			// ... find first i where i != j + 2k^2
+			continue CompositeLoop
+		}
 		fmt.Println("Result:", i)
-		break
+		return
 	}
 }

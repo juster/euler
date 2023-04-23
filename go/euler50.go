f237e400ea614da20268e0919b9f2f644a36c563
Justin Davis <jrcd83@gmail.com>
2023-04-23 12:18:56 -0400

    50: Use sums intelligently
    We can use the sums to remove preceeding primes and add successive primes to the
    sequence en-masse.

--- go/euler50.go
+++ go/euler50.go
@@ -1,29 +1,42 @@
 package main
+
 import "fmt"
-const limit = 1000000
-var primes = eratosthenes(limit).primes()
+
+const limit = 1_000_000
+
+var sieve = eratosthenes(limit)
+var sum = []int{0} // allows us a no-op for pivoting
+
 func main() {
-	var y, n int
-	for _, i := range primes {
-		var x, m int
-		for _, j := range primes {
-			m++
-			x += j
-			if x == i && m > n {
-				y = x
-				n = m
-				break
-			}
-			if x >= i {
-				break
+	x := sum[0]
+	for i := range sieve.genPrimes(1) {
+		x += i
+		if x > limit {
+			break
+		}
+		sum = append(sum, x)
+	}
+
+	// Loop through the sums, pivoting around j. We subtract i primes from
+	// the beginning and add k primes to the end. Sum for the k succeeding
+	// primes is sum[k] - sum[j].
+
+	var n, a, b int
+	for i := 0; i < len(sum); i++ {
+		for j := i; j < len(sum); j++ {
+		LoopK:
+			for k := j; k < len(sum); k++ {
+				x := sum[j] - sum[i] + (sum[k] - sum[j])
+				switch {
+				case x > limit:
+					break LoopK
+				case !sieve.isPrime(x):
+					continue
+				case k-i > b-a:
+					n, a, b = x, i, k
+				}
 			}
 		}
 	}
-	x := 0
-	for _, p := range primes {
-		if x >= y { break }
-		fmt.Println(p)
-		x += p
-	}
-	fmt.Println(y, n)
+	fmt.Println(n, a, b)
 }

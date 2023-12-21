7a061e5a376d20a0394a4af9b190168ff310cd75
Justin Davis <jrcd83@gmail.com>
2023-12-21 10:21:08 -0800

    55: Start over with simple brute force

--- go/euler55.go
+++ go/euler55.go
@@ -2,133 +2,97 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"math/big"
-	"strconv"
-	"strings"
 )
 
 const (
 	input_digits = 4
-	recurse_max = 50
+	recurse_max  = 50
+	loop_max     = 10000
 )
 
-type state struct {
-	stack []*big.Int
-	cache map[string]bool
+var (
+	count, top int
+	stack      [2 * recurse_max]big.Int
+	cache      map[string]bool
+)
+
+func init() {
+	cache = make(map[string]bool)
 }
 
-func NewState() *state {
-	return &state{
-		make([]*big.Int, 2 * recurse_max),
-		make(map[string]bool),
+func push(x, y *big.Int) {
+	stack[top+0] = *x
+	stack[top+1] = *y
+	top += 2
+}
+
+// Cache whether a palindrome was found.
+func store(found bool) {
+	if found {
+		for _, x := range stack[:top] {
+			cache[x.String()] = true
+		}
+	} else {
+		count++
 	}
-}
-
-func (s *state) Push(x *big.Int) {
-	n := len(s.stack)
-	s.stack = s.stack[:n+1]
-	s.stack[n] = x
-}
-
-func (s *state) Finish(lychrel bool) {
-	for _, y := range s.stack {
-		s.cache[y.String()] = lychrel
-	}
-	s.stack = s.stack[:0]
+	top = 0
 }
 
 func main() {
-	state := NewState()
-	in := pairs(input_digits)
-	n := 0
-OuterLoop:
-	for p := range in {
-		a := p.a
-		b := p.b
-	InnerLoop:
-		for i := 0; i < recurse_max; i++ {
-			c := &big.Int{}
-			c.Add(a, b)
-
-			s := c.String()
-			if lychrel, seen := state.cache[s]; seen {
-				if lychrel {
-					n++
+	for i := int64(1); i < loop_max; i++ {
+		var pal bool
+		x := big.NewInt(i)
+	Loop:
+		for j := 0; j < recurse_max; j++ {
+			s := x.String()
+			if pal = cache[s]; pal && top > 0 {
+				// Don't stop if the operands are palindromes. The sum must be a palindrome
+				// to reject it as a Lychrael number.
+				break Loop
+			}
+			var y *big.Int
+			y, pal = reverse(x)
+			push(x, y)
+			if pal {
+				if top > 2 {
+					//fmt.Println("*DBG* palindrome:", i, j, y)
+					break Loop
 				}
-				state.Finish(lychrel)
-				continue OuterLoop
 			}
-			if s[len(s)-1] == '0' {
-				state.cache[s] = true
-				break InnerLoop
-			}
-			if isPalindrome(s) {
-				fmt.Println(p.a, p.b, c)
-				state.cache[s] = false
-				state.Finish(false)
-				continue OuterLoop
-			}
-
-			state.Push(a)
-			state.Push(b)
-			a = c
-			b = reverse(a)
+			x = x.Add(x, y)
 		}
-		state.Finish(true)
-		fmt.Println(p.a, p.b, "(found)")
-		n++
-		if a.Cmp(b) != 0 {
-			n++
+		if !pal {
+			fmt.Println("Found", i)
 		}
+		store(pal)
 	}
-	fmt.Println("Result", n)
+	fmt.Println("Answer", count, len(cache))
 }
 
-type pair struct {
-	a, b *big.Int
-}
-
-func pairs(digits int) <-chan *pair  {
-	out := make(chan *pair)
-	go func() {
-		for n := 1; n <= digits; n++ {
-			for i := 1; i <= 9; i++ {
-				x, _ := strconv.Atoi(strings.Repeat("1", n))
-				beg := i * x
-				top := (i + 1) * int(math.Pow(10.0, float64(n-1)))
-				//fmt.Println(beg, top, top-beg)
-
-				for j := beg; j < top; j++ {
-					if j % 10 == 0 {
-						continue
-					}
-					a := big.NewInt(int64(j))
-					b := reverse(a)
-					out <- &pair{a, b}
-				}
-			}
-		}
-		close(out)
-	}()
-	return out
-}
-
-func reverse(x *big.Int) *big.Int {
+func reverse(x *big.Int) (*big.Int, bool) {
 	s := x.String()
 	buf := make([]byte, len(s))
-	for i := 0; i < len(s); i++ {
-		buf[len(s) - i - 1] = s[i]
-	}
-	y, _ := strconv.Atoi(string(buf))
-	return big.NewInt(int64(y))
-}
+	pal := true
 
-func isPalindrome(s string) bool {
 	for i := 0; i < len(s)/2; i++ {
-		if s[i] != s[len(s)-i-1] {
-			return false
+		j := len(s) - i - 1
+		if s[i] != s[j] {
+			pal = false
 		}
+		buf[j] = s[i]
+		buf[i] = s[j]
 	}
-	return true
+	if len(s)%2 == 1 {
+		j := len(s) / 2
+		buf[j] = s[j]
+	}
+
+	y := &big.Int{}
+	_, ok := y.SetString(string(buf), 10)
+	if !ok {
+		panic("internal error: " + string(buf))
+	}
+
+	return y, pal
 }

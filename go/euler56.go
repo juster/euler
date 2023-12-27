92c1343a8ce2269af7c8d641b34ff894fc3bd1e4
Justin Davis <jrcd83@gmail.com>
2023-12-27 12:35:15 -0800

    56: Simple brute force

---
+++ go/euler56.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+)
+
+const limit = 100
+
+func main() {
+	start := time.Now()
+	var max int
+	var x, y big.Int
+	for i := int64(2); i < limit; i++ {
+		x.SetInt64(i)
+		y.Set(&x)
+		for j := 2; j < limit; j++ {
+			y.Mul(&x, &y)
+			if sum := digitalSum(&y); sum > max {
+				max = sum
+			}
+		}
+	}
+	fmt.Printf("Answer: %d in %s\n", max, time.Now().Sub(start))
+}
+
+func digitalSum(x *big.Int) int {
+	var sum int
+	s := x.String()
+	for i := 0; i < len(s); i++ {
+		sum += int(s[i]) - '0'
+	}
+	return sum
+}
